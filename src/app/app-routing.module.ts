86f16f311c1c4118599f069135f8d26eec6d1156
ValdirCezar <valdircezar312@gmail.com>
2021-07-01 12:57:05 -0300

    Rota para componente HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent
+    path: '', component: NavComponent, children: [
+      { path: 'home', component: HomeComponent }
+    ]
   }
 ];
 
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.navigate(['home'])
   }
 
 }
