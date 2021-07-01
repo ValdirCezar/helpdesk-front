f896ac07a5cbbf5168d1e6a0e57970631042718e
ValdirCezar <valdircezar312@gmail.com>
2021-07-01 08:57:10 -0300

    Componente NAV criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,9 +27,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+// Componentes do projeto
+import { NavComponent } from './components/nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
