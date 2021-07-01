40d4b4b6cecdb79eba6fee44dd27244505fabd4b
ValdirCezar <valdircezar312@gmail.com>
2021-07-01 14:58:35 -0300

    Componente HEADER criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,12 +30,14 @@ import { MatCardModule } from '@angular/material/card';
 // Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
