99e4bc89ff3ea236fd5abdf11eadb9927ab37640
ValdirCezar <valdircezar312@gmail.com>
2021-07-02 10:12:04 -0300

    Rota para LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
