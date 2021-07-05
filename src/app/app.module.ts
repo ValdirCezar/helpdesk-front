8a06c2ea5467aa780900cb503fd0cff69473c55d
ValdirCezar <valdircezar312@gmail.com>
2021-07-05 15:55:43 -0300

    interceptor para requisições http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -74,7 +75,7 @@ import { ToastrModule } from 'ngx-toastr';
       progressBar: true
     })
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token = localStorage.getItem('token');
+
+    if (token) {
+      const cloneReq = 
+        request.clone({ headers: request.headers.set('Authorization', `Bearer ${token}`) });
+        return next.handle(cloneReq);
+    } else {
+      return next.handle(request);
+    }
+  }
+}
+
+export const AuthInterceptorProvider = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+]
