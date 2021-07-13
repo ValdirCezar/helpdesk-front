ceb39a956e9260b09ec656b62b77a1f86e902aa1
ValdirCezar <valdircezar312@gmail.com>
2021-07-13 10:05:50 -0300

    Componente CHAMADO-READ criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 @NgModule({
   declarations: [
@@ -64,7 +65,8 @@ import { ChamadoUpdateComponent } from './components/chamado/chamado-update/cham
     ClienteUpdateComponent,
     ChamadoListComponent,
     ChamadoCreateComponent,
-    ChamadoUpdateComponent
+    ChamadoUpdateComponent,
+    ChamadoReadComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-read',
+  templateUrl: './chamado-read.component.html',
+  styleUrls: ['./chamado-read.component.css']
+})
+export class ChamadoReadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
