b4fbd3e4a2298908d05a0b26df161089cae80d62
ValdirCezar <valdircezar312@gmail.com>
2021-07-12 23:24:21 -0300

    Componente CHAMADO-UPDATE criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
 @NgModule({
   declarations: [
@@ -62,7 +63,8 @@ import { ChamadoCreateComponent } from './components/chamado/chamado-create/cham
     ClienteListComponent,
     ClienteUpdateComponent,
     ChamadoListComponent,
-    ChamadoCreateComponent
+    ChamadoCreateComponent,
+    ChamadoUpdateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-update',
+  templateUrl: './chamado-update.component.html',
+  styleUrls: ['./chamado-update.component.css']
+})
+export class ChamadoUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
