7228ad45db98e25d8d7e585d56b2169dc8e14b7a
ValdirCezar <valdircezar312@gmail.com>
2021-07-12 23:25:59 -0300

    Rota para componente CHAMADO-UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
@@ -31,8 +32,9 @@ const routes: Routes = [
       { path: 'clientes/update/:id', component: ClienteUpdateComponent },
       { path: 'clientes/delete/:id', component: ClienteDeleteComponent },
 
-      { path: 'chamados',                   component:     ChamadoListComponent },
-      { path: 'chamados/create',            component:   ChamadoCreateComponent },
+      { path: 'chamados',                       component:     ChamadoListComponent },
+      { path: 'chamados/create',                component:   ChamadoCreateComponent },
+      { path: 'chamados/update/:id',            component:   ChamadoUpdateComponent },
     ]
   }
 ];
